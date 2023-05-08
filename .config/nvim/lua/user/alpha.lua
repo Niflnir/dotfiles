4f0b5722d158d5e4980fc189b9e1600170b84b3a
Niflnir <shaunchenggy@gmail.com>
2023-05-08 22:51:23 +0800

    added neovide

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -15,7 +15,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find sessions", ":SearchSession <CR>"),
+	dashboard.button("p", "  Find projects", ":Telescop projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/dotfiles/.config/nvim/init.lua <CR>"),
---
+++ .config/nvim/lua/user/neovide.lua
@@ -0,0 +1,7 @@
+if vim.g.neovide then
+	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h8"
+	vim.g.neovide_cursor_animation_length = 0.05
+	vim.g.neovide_cursor_trail_size = 0.2
+	vim.g.neovide_cursor_antialiasing = true
+	vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
